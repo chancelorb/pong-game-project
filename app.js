f95f1a02872a42865ac3cae7ef805855dfbeee18
Chance Baars <chance@ool-ad020102.dyn.optonline.net>
2018-03-30 15:41:04 -0400

    detect when ball is out of the field and alert

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ $(document).ready(function() {
 
   };
   const field = {
-    width: 500,
+    width: 243,
     height: 300,
   };
 
@@ -26,63 +26,39 @@ $(document).ready(function() {
     redPaddle.position = redPaddle.name[0].getBoundingClientRect();
 
     if (ball.start === 1) {
-      $('#ball').css('left', '-=1px');// interval and with small jumps
-      console.log(ball.position)
+      $('#ball').css('left', '-=10px');// interval and with small jumps
+      field.width -= 10;
       if (ball.position.x < bluePaddle.position.x + bluePaddle.position.width &&
          ball.position.x + ball.position.width > bluePaddle.position.x &&
          ball.position.y < bluePaddle.position.y + bluePaddle.position.height &&
          ball.position.height + ball.position.y > bluePaddle.position.y) {
-          console.log('Touched Blue')
+          console.log(ball.position.x);
           ball.start = 2;
-      };
+      } else if (ball.position.x < 250) {
+        alert('red wins');
+        ball.start = 0;
+        ballStartPosition();
+      }
     };
     if (ball.start === 2) {
-      $('#ball').css('left', '+=1px');
+      $('#ball').css('left', '+=10px');
       if (ball.position.x < redPaddle.position.x + redPaddle.position.width &&
          ball.position.x + ball.position.width > redPaddle.position.x &&
          ball.position.y < redPaddle.position.y + redPaddle.position.height &&
          ball.position.height + ball.position.y > redPaddle.position.y) {
-          console.log('Touched Red');
+          console.log(ball.position.x);
           ball.start = 1;
+      }else if (ball.position.x > 750) {
+        alert('blue wins');
+        ball.start = 0;
+        ballStartPosition();
       };
     };
-    //shootBall();
   }, 20)
 
-
-  /*function shootBall() {
-    if (ball.start === 1) {
-      $('#ball').css('left', ball.position.x + "-=5px")// interval and with small jumps
-      if (ball.position.x < bluePaddle.position.x + bluePaddle.position.width &&
-         ball.position.x + ball.position.width > bluePaddle.position.x &&
-         ball.position.y < bluePaddle.position.y + bluePaddle.position.height &&
-         ball.position.height + ball.position.y > bluePaddle.position.y) {
-          console.log('Touched Blue')
-          ball.start = 2;
-      };
-    };
-    if (ball.start === 2) {
-      $('#ball').css('left', ball.position.x + "+=5px")
-      if (ball.position.x < redPaddle.position.x + redPaddle.position.width &&
-         ball.position.x + ball.position.width > redPaddle.position.x &&
-         ball.position.y < redPaddle.position.y + redPaddle.position.height &&
-         ball.position.height + ball.position.y > redPaddle.position.y) {
-          console.log('Touched Red');
-          ball.start = 1;
-      };
-    };
-    if (ball.start === 3) {
-      if (ball.position.x < redPaddle.position.x + redPaddle.position.width &&
-         ball.position.x + ball.position.width > redPaddle.position.x &&
-         ball.position.y < redPaddle.position.y + redPaddle.position.height &&
-         ball.position.height + ball.position.y > redPaddle.position.y) {
-          direction.x *= (-1);
-          direction.speed += 10;
-          console.log('Touched Red');
-      };
-    };
-  } */
-
+  function ballStartPosition() {
+    $('#ball').css('left', '243px')
+  }
 
   $(document).on('keydown', function(e) {
     if (e.which === 65 && bluePaddle.upDown > 8) { //paddle up and down
@@ -94,11 +70,9 @@ $(document).ready(function() {
     } else if (e.which === 38 && redPaddle.upDown > 8) {
       redPaddle.name.animate({'top': '-=25px'}, 1);
       redPaddle.upDown -= 1;
-      console.log(redPaddle.upDown);
     } else if (e.which === 40 && redPaddle.upDown < 18) {
       redPaddle.name.animate({'top': '+=25px'}, 1)
       redPaddle.upDown += 1;
-      console.log(redPaddle.upDown);
     } else if (e.which === 32) {
       //start ball
       ball.start = 1;//to start the game.
