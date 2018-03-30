def1414078b0acb7d8be4790bc06c6ededaca4a4
Chance Baars <chance@ool-ad020102.dyn.optonline.net>
2018-03-30 13:53:24 -0400

    ball moves between paddles

--- app.js
+++ app.js
@@ -7,10 +7,7 @@ $(document).ready(function() {
 
   const ball = {
     name: $('#ball'),
-    start: 0,
-    left: 243,
-    top: 150,
-
+    start: 0, //0=not playing 1=just pressed start 2=last touched by blue 3= last touched by red 4=display winner
   }
 
   const redPaddle = {
@@ -22,67 +19,91 @@ $(document).ready(function() {
     width: 500,
     height: 300,
   };
-  const direction = {
-    x: "",
-    speed: 200,
-
-  }
-
 
   setInterval(function(){//check for collision and update
     ball.position = ball.name[0].getBoundingClientRect();
     bluePaddle.position = bluePaddle.name[0].getBoundingClientRect();
     redPaddle.position = redPaddle.name[0].getBoundingClientRect();
 
-    if (ball.position.x < bluePaddle.position.x + bluePaddle.position.width &&
-       ball.position.x + ball.position.width > bluePaddle.position.x &&
-       ball.position.y < bluePaddle.position.y + bluePaddle.position.height &&
-       ball.position.height + ball.position.y > bluePaddle.position.y) {
-        direction.x = '+=50';
-        direction.speed += 10;
-        console.log('Touched Blue')
+    if (ball.start === 1) {
+      $('#ball').css('left', '-=1px');// interval and with small jumps
+      console.log(ball.position)
+      if (ball.position.x < bluePaddle.position.x + bluePaddle.position.width &&
+         ball.position.x + ball.position.width > bluePaddle.position.x &&
+         ball.position.y < bluePaddle.position.y + bluePaddle.position.height &&
+         ball.position.height + ball.position.y > bluePaddle.position.y) {
+          console.log('Touched Blue')
+          ball.start = 2;
+      };
     };
-
-    if (ball.position.x < redPaddle.position.x + redPaddle.position.width &&
-       ball.position.x + ball.position.width > redPaddle.position.x &&
-       ball.position.y < redPaddle.position.y + redPaddle.position.height &&
-       ball.position.height + ball.position.y > redPaddle.position.y) {
-        direction.x = '-=50';
-        direction.speed += 10;
-        console.log('Touched Red');
+    if (ball.start === 2) {
+      $('#ball').css('left', '+=1px');
+      if (ball.position.x < redPaddle.position.x + redPaddle.position.width &&
+         ball.position.x + ball.position.width > redPaddle.position.x &&
+         ball.position.y < redPaddle.position.y + redPaddle.position.height &&
+         ball.position.height + ball.position.y > redPaddle.position.y) {
+          console.log('Touched Red');
+          ball.start = 1;
+      };
     };
-    shootBall();
+    //shootBall();
   }, 20)
 
 
-  function shootBall() {
-    if (ball.start === 5) {
-
-      ball.name.animate({'left': direction.x});// interval and with small jumps
-
+  /*function shootBall() {
+    if (ball.start === 1) {
+      $('#ball').css('left', ball.position.x + "-=5px")// interval and with small jumps
+      if (ball.position.x < bluePaddle.position.x + bluePaddle.position.width &&
+         ball.position.x + ball.position.width > bluePaddle.position.x &&
+         ball.position.y < bluePaddle.position.y + bluePaddle.position.height &&
+         ball.position.height + ball.position.y > bluePaddle.position.y) {
+          console.log('Touched Blue')
+          ball.start = 2;
+      };
     };
-  }
+    if (ball.start === 2) {
+      $('#ball').css('left', ball.position.x + "+=5px")
+      if (ball.position.x < redPaddle.position.x + redPaddle.position.width &&
+         ball.position.x + ball.position.width > redPaddle.position.x &&
+         ball.position.y < redPaddle.position.y + redPaddle.position.height &&
+         ball.position.height + ball.position.y > redPaddle.position.y) {
+          console.log('Touched Red');
+          ball.start = 1;
+      };
+    };
+    if (ball.start === 3) {
+      if (ball.position.x < redPaddle.position.x + redPaddle.position.width &&
+         ball.position.x + ball.position.width > redPaddle.position.x &&
+         ball.position.y < redPaddle.position.y + redPaddle.position.height &&
+         ball.position.height + ball.position.y > redPaddle.position.y) {
+          direction.x *= (-1);
+          direction.speed += 10;
+          console.log('Touched Red');
+      };
+    };
+  } */
 
 
   $(document).on('keydown', function(e) {
-    if (e.which === 65 && bluePaddle.upDown > 5) { //paddle up and down
-      bluePaddle.name.animate({'top': '-=15px'}, 1);
+    if (e.which === 65 && bluePaddle.upDown > 8) { //paddle up and down
+      bluePaddle.name.animate({'top': '-=25px'}, 1);
       bluePaddle.upDown -= 1;
-    } else if (e.which === 90 && bluePaddle.upDown < 21) {
-      bluePaddle.name.animate({'top': '+=15px'}, 1);
+    } else if (e.which === 90 && bluePaddle.upDown < 18) {
+      bluePaddle.name.animate({'top': '+=25px'}, 1);
       bluePaddle.upDown += 1;
-    } else if (e.which === 38 && redPaddle.upDown > 5) {
-      redPaddle.name.animate({'top': '-=15px'}, 1);
+    } else if (e.which === 38 && redPaddle.upDown > 8) {
+      redPaddle.name.animate({'top': '-=25px'}, 1);
       redPaddle.upDown -= 1;
-    } else if (e.which === 40 && redPaddle.upDown < 21) {
-      redPaddle.name.animate({'top': '+=15px'}, 1)
+      console.log(redPaddle.upDown);
+    } else if (e.which === 40 && redPaddle.upDown < 18) {
+      redPaddle.name.animate({'top': '+=25px'}, 1)
       redPaddle.upDown += 1;
+      console.log(redPaddle.upDown);
     } else if (e.which === 32) {
       //start ball
-      ball.start = 5;//to know if the game is playing
+      ball.start = 1;//to start the game.
 
     }
   })
-
 
 });
